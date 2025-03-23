6eb988919ed607aab974358ee4ceee2d51cc9bc8
evvgeniy978 <evvgeniy978@gmail.com>
2025-03-23 18:01:03 +0100

    adress-book-06

---
+++ adress_book.py
@@ -0,0 +1,55 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        found = False
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                found = True
+                break
+        if not found:
+            raise ValueError(f"Phone {old_phone} not found")
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name}, phones: {', '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def find(self, name):
+        return self.data.get(name)
